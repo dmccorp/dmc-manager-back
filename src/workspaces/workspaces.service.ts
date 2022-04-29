a04744b63215d2a47781e54fb7268b1f19fd41c7
Basith Kunimal <basith@leena.ai>
2022-04-29 20:01:28 +0530

    workspace resource

---
+++ src/workspaces/workspaces.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { CreateWorkspaceDto } from './dto/create-workspace.dto';
+import { UpdateWorkspaceDto } from './dto/update-workspace.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Workspace } from './entities/workspace.entity';
+
+@Injectable()
+export class WorkspacesService {
+  constructor(
+    @InjectRepository(Workspace)
+    private workspacesRepository: Repository<Workspace>,
+  ) {}
+
+  create(createWorkspaceDto: CreateWorkspaceDto) {
+    return this.workspacesRepository.save(createWorkspaceDto);
+  }
+
+  findAll(): Promise<Workspace[]> {
+    return this.workspacesRepository.find();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} workspace`;
+  }
+
+  update(id: number, updateWorkspaceDto: UpdateWorkspaceDto) {
+    return `This action updates a #${id} workspace`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} workspace`;
+  }
+}
