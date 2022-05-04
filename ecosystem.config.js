a8f204e768c4846e1dc61d7381514e8f756a6780
Basith Kunimal <basith@leena.ai>
2022-05-04 16:26:50 +0530

    add pm2 deploy file

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'dmc-manage-nest',
+      script: 'npm run start:dev',
+      watch: '.',
+    },
+  ],
+  deploy: {
+    development: {
+      user: 'basith',
+      host: '34.100.183.103',
+      ref: 'origin/master',
+      repo: 'git@github.com:dmccorp/dmc-manager-back',
+      path: '/home/basith/dmc-manage-nest',
+      'post-deploy': 'npm install',
+    },
+  },
+};
