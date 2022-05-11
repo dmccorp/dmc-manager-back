fb69ab88aea3462ff64b97765dc45180ef51df45
roshinhanjas <roshin.hanjas@luxuryescapes.com>
2022-05-11 20:38:57 +0530

    pm2 deploy dev added

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,5 +15,13 @@ module.exports = {
       path: '/home/basith/dmc-manage-nest',
       'post-deploy': 'npm install',
     },
+    dev: {
+      user: 'roshanhanjas',
+      host: '34.100.183.103',
+      ref: 'origin/master',
+      repo: 'git@github.com:dmccorp/dmc-manager-back',
+      path: '/home/basith/dmc-manage-nest',
+      'post-deploy': 'npm install',
+    },
   },
 };
